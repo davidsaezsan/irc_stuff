46a5408800ccca0d1a05e4b0ccb39e27a8c4643a
David Saez <davidsaezsan@users.noreply.github.com>
2023-05-02 15:52:23 -0400

    Add files via upload
    Extract structures from a Gaussian 16 IRC.

---
+++ extr.py
@@ -0,0 +1,23 @@
+import re
+
+# Read the Gaussian output file
+with open('gaussian_output.log', 'r') as f:
+    output = f.read()
+
+# Define a regular expression pattern to match the converged geometries
+pattern = re.compile(r'Input orientation:[\s\S]*?(?=Rotational constants)')
+
+# Find all matches in the output file
+matches = pattern.findall(output)
+
+# Write each match to a separate file
+for i, match in enumerate(matches):
+    with open(f'converged_geometry_{i+1}.xyz', 'w') as f:
+        # Extract the atom coordinates from the match
+        atom_lines = match.split('\n')[5:-41]
+        #Delete columns 0 and 2 without relevant info
+        atom_lines2=[atom_lines[j].split()[1]+' '+atom_lines[j].split()[3]+' '+atom_lines[j].split()[4]+' '+atom_lines[j].split()[5] for j in range(len(atom_lines))]
+        coords = '\n'.join(atom_lines2)
+        
+        # Write the coordinates to the output file
+        f.write(f'{len(atom_lines)}\n\n{coords}\n')
